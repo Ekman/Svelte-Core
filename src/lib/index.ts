61b8ecb1af2a00474c10a7bd522505287dc3fe35
Niklas Ekman <nikl.ekman@gmail.com>
2025-06-21 22:04:27 +0200

    Export props

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export { default as GoogleAnalytics } from "./google-analytics.svelte";
+export * from "./google-analytics.svelte";
 export * from "./google-analytics.js";
 export { default as Seo } from "./seo.svelte";
+export * from "./seo.svelte";
