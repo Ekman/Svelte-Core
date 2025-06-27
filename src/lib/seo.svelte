7c53baead8dce917bee1dca131f802320d4f35f7
Niklas Ekman <nikl.ekman@gmail.com>
2025-06-27 11:19:14 +0200

    Icon sizes

--- src/lib/seo.svelte
+++ src/lib/seo.svelte
@@ -6,14 +6,21 @@
 		readonly queryParams?: QueryParams;
 	}
 
+	export interface SeoIcon {
+		readonly href: string;
+		readonly height: number;
+		readonly width: number;
+		readonly mimeContentType: string;
+	}
+
 	export interface SeoProps {
 		readonly siteTitle: string;
 		readonly pageTitle: string;
 		readonly description: string;
-		readonly icon: string;
+		readonly origin: string;
+		readonly icon: SeoIcon;
 		readonly canonical?: SeoCanonical;
 		readonly next?: SeoCanonical;
-		readonly origin: string;
 	}
 
 	const { siteTitle, description, pageTitle, icon, canonical, next, origin }: SeoProps = $props();
@@ -61,11 +68,11 @@
 	<meta property="og:site_name" content={siteTitle} />
 	<meta property="og:title" content={pageTitle} />
 	<meta property="og:description" content={description} />
-	<meta property="og:image" content={icon} />
-	<meta property="og:image:secure_url" content={icon} />
-	<meta property="og:image:width" content="1024" />
-	<meta property="og:image:height" content="1024" />
-	<meta property="og:image:type" content="image/png" />
+	<meta property="og:image" content={icon.href} />
+	<meta property="og:image:secure_url" content={icon.href} />
+	<meta property="og:image:width" content={"" + icon.width} />
+	<meta property="og:image:height" content={"" + icon.height} />
+	<meta property="og:image:type" content={icon.mimeContentType} />
 	<meta property="og:image:alt" content={`${siteTitle} logo`} />
 	<meta property="og:url" content={canonicalUrl} />
 	<meta property="og:type" content="website" />
@@ -73,6 +80,6 @@
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:title" content={pageTitle} />
 	<meta name="twitter:description" content={description} />
-	<meta name="twitter:image" content={icon} />
+	<meta name="twitter:image" content={icon.href} />
 	<meta name="twitter:creator" content="@iamBraska" />
 </svelte:head>
