5d2acf46d778962380086409fbf922c5cd87effa
Niklas Ekman <nikl.ekman@gmail.com>
2025-07-08 20:59:36 +0200

    Export props from DateTime

--- src/lib/date-time.svelte
+++ src/lib/date-time.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import {localeFormatDate} from "$lib/locale.js";
 
-	interface Props {
+	export interface DateTimeProps {
 		readonly date: Date;
 		readonly locale?: string;
 	}
 
-	const { date, locale }: Props = $props();
+	const { date, locale }: DateTimeProps = $props();
 
 	const isoString = $derived(date.toISOString());
 	const formatted = $derived(localeFormatDate(date, locale));
