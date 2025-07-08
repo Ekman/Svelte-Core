520c6954736d2b8558987c7ec0d7570526681d10
Niklas Ekman <nikl.ekman@gmail.com>
2025-07-08 20:59:10 +0200

    Properly export DateTime

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,10 @@
+export * from "./google-analytics.js";
+
 export { default as GoogleAnalytics } from "./google-analytics.svelte";
 export * from "./google-analytics.svelte";
-export * from "./google-analytics.js";
+
 export { default as Seo } from "./seo.svelte";
 export * from "./seo.svelte";
+
 export { default as DateTime } from "./date-time.svelte";
+export * from "./date-time.svelte";
