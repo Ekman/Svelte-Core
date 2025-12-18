88bb749357b723997440fd6f34f12a4f0dbea358
Niklas Ekman <nikl.ekman@gmail.com>
2025-12-18 21:22:15 +0100

    Automatically add utm_source to all external links. Add dofollow links

--- src/lib/external-link.svelte
+++ src/lib/external-link.svelte
@@ -1,15 +1,27 @@
 <script lang="ts">
 	import type {Snippet} from "svelte";
+	import {page} from "$app/state";
 
 	export interface ExternalLinkProps {
 		readonly href: string;
 		readonly children: Snippet;
-		readonly type?: "nofollow" | "ugc" | "sponsored";
+		readonly type?: "dofollow" | "nofollow" | "ugc" | "sponsored";
+		readonly origin?: string;
 	}
 
-	const { href, children, type = "nofollow" }: ExternalLinkProps = $props();
+	const { href, children, type = "nofollow", origin }: ExternalLinkProps = $props();
+
+	const source = $derived(origin ?? page.url.origin);
+
+	const hrefWithSource = $derived.by(() => {
+		const sign = href.includes("?") ? "&" : "?";
+		return href + sign + `utm_source=${encodeURIComponent(source)}`;
+	});
+
+	const rel = $derived(type === "dofollow" ? undefined : type);
 </script>
 
-<a {href} target="_blank" rel="{type} noreferrer noopener">
+<a href={hrefWithSource}
+	 target="_blank" rel="{rel} noreferrer noopener">
 	{@render children()}
 </a>
